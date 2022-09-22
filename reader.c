18df249b3f75dd03f6f856dea596eb8d2cf3e2c2
juanignava <ju.navarro@estudiantec.cr>
2022-09-22 15:22:44 -0600

    [BUG] Fixed the bug with the functionality of the semaphores
    The program is sending multiple data through a shared memory and
    using the technique of a circular list.

--- reader.c
+++ reader.c
@@ -12,111 +12,60 @@
 #include <sys/mman.h>
 #include <sys/wait.h>
 
-// STRUCTS
+#define NUM_ITEMS 120
 
-typedef struct {
-  int *array;
-  int length;
-  int next_in;
-  int next_out;
-  Semaphore *mutex;
-  Semaphore *items;
-  Semaphore *spaces;
-} Queue;
+// GLOBALS
 
-typedef struct {
-  Queue *queue;
-} Shared;
+int chunk = 10;
 
-// GLOBAL
+// STRUCT
+typedef struct
+{
+    int value;
+} QueueData;
 
-// number of elements in the array
-int N = 128;
+
+typedef struct
+{
+    sem_t sem_filled;
+    sem_t sem_empty;
+    int next_input;
+    int next_output;
+} QueueInfo;
+
 
 // FUNCTIONS
-Queue *make_queue(int length)
+void read_info(QueueData *queue, QueueInfo *queue_info)
 {
-  Queue *queue = (Queue *) malloc(sizeof(Queue));
-  queue->length = length;
-  queue->array = (int *) malloc(length * sizeof(int));
-  queue->next_in = 0;
-  queue->next_out = 0;
-  queue->mutex = make_semaphore(1);
-  queue->items = make_semaphore(0);
-  queue->spaces = make_semaphore(length-1);
-  return queue;
+    for (int i = 0; i < NUM_ITEMS; i++)
+    {
+        sem_wait(&queue_info->sem_empty);
+        int val = queue[queue_info->next_output].value;
+        printf("Reading value: %d\n", val);
+        queue_info->next_output = (i+1) % chunk; // for circular list
+        sem_post(&queue_info->sem_filled);
+    }
+    
 }
 
-int queue_incr(Queue *queue, int i)
+int main()
 {
-  return (i+1) % queue->length;
-}
-
-int queue_empty(Queue *queue)
-{
-  // queue is empty if next_in and next_out are the same
-  int res = (queue->next_in == queue->next_out);
-  return res;
-}
-
-int queue_full(Queue *queue)
-{
-  // queue is full if incrementing next_in lands on next_out
-  int res = (queue_incr(queue, queue->next_in) == queue->next_out);
-  return res;
-}
-
-void queue_push(Queue *queue, int item) {
-  semaphore_wait(queue->spaces);
-  semaphore_wait(queue->mutex);
-
-  queue->array[queue->next_in] = item;
-  queue->next_in = queue_incr(queue, queue->next_in);
-
-  semaphore_signal(queue->mutex);
-  semaphore_signal(queue->items);
-}
-
-int queue_pop(Queue *queue) {
-  semaphore_wait(queue->items);
-  semaphore_wait(queue->mutex);
-  
-  int item = queue->array[queue->next_out];
-  queue->next_out = queue_incr(queue, queue->next_out);
-
-  semaphore_signal(queue->mutex);
-  semaphore_signal(queue->spaces);
-
-  return item;
-}
-
-
-
-void *reader_entry(void *arg)
-{
-  int i;
-  int item;
-  Shared *shared = (Shared *) arg;
-
-  for (i=0; i<N; i++) {
-    printf("Before pop\n");
-    item = queue_pop(shared->queue);
-    printf("consuming item %d\n", item);
-  }
-  //pthread_exit(NULL);
-}
-
-int
-main()
-{
-    /*
-    // open the file descriptor, show an error if it does not exist
-    int fd = open("/tmp/project_1",O_RDWR,0644);
-    if (fd < 0) {
-        perror("project_1");
+    // opens the file descriptor that has to be mapped to the
+    //     shared memory
+    int fd_queue = open("/tmp/project_1_queue", O_RDWR | O_CREAT, 0644);
+    if (fd_queue < 0) {
+        perror("project_1_queue");
         exit(1);
     }
-
+    ftruncate(fd_queue, chunk*sizeof(QueueData));
+    
+    int fd_info = open("/tmp/project_1_info", O_RDWR | O_CREAT, 0644);
+    if (fd_info < 0) {
+        perror("project_1_info");
+        exit(1);
+    }
+    ftruncate(fd_info, sizeof(QueueInfo));
+    
     // memory mapping function instance
     //      address: NULL means the kernel can place the mapping anywhere it sees fit
     //      lenght: number of bytes to be mapped, the array will contain 10 integers
@@ -127,23 +76,19 @@ main()
     //      offset: 0, offset from where the mapping started
 
     // source: https://linuxhint.com/using_mmap_function_linux/
+    QueueData *queue =  mmap(NULL, chunk*sizeof(QueueData), PROT_READ | PROT_WRITE,
+        MAP_SHARED, fd_queue, 0);
     
-    Shared *shared = mmap(NULL, sizeof(Shared)+sizeof(Queue)+10*sizeof(int), PROT_READ | PROT_WRITE,
-        MAP_SHARED, fd, 0);
-    //= mmap(NULL, sizeof(Shared), PROT_READ | PROT_WRITE,
-      //  MAP_SHARED, fd, 0);
+    QueueInfo *queue_info = mmap(NULL, sizeof(QueueInfo), PROT_READ | PROT_WRITE,
+        MAP_SHARED, fd_info, 0);
+
+
+    // fill the queue with the data
+    //printf("Antes del llamado de la función");
+    read_info(queue, queue_info);
     
-    //shared = make_shared();
-
-    reader_entry(shared);
-
-    // close the file descriptor
-    close(fd);
-
-    // stop the shared memory
-    */
-    unlink("/tmp/project_1_queue");
-    unlink("/tmp/project_1_info");
-
+   
+    close(fd_queue);
+    close(fd_info);
     return 0;
 }
--- writer.c
+++ writer.c
@@ -24,36 +24,49 @@ typedef struct
     int value;
 } QueueData;
 
+
 typedef struct
 {
-    sem_t *sem_filled;
-    sem_t *sem_empty;
+    sem_t sem_filled;
+    sem_t sem_empty;
     int next_input;
     int next_output;
 } QueueInfo;
 
 
 // FUNCTIONS
-void *write_info(QueueData *queue, QueueInfo *queue_info)
+void write_info(QueueData *queue, QueueInfo *queue_info)
 {
     for (int i = 0; i < NUM_ITEMS; i++)
     {
-        sem_wait(queue_info->sem_filled);
+        sem_wait(&queue_info->sem_filled);
+        printf("Adding value: %d\n", i);
         queue[queue_info->next_input].value = i;
         queue_info->next_input = (i+1) % chunk; // for circular list
-        sem_post(queue_info->sem_empty);
+        sem_post(&queue_info->sem_empty);
     }
     
 }
+
 
 int main()
 {
     // opens the file descriptor that has to be mapped to the
     //     shared memory
     int fd_queue = open("/tmp/project_1_queue", O_RDWR | O_CREAT, 0644);
+    if (fd_queue < 0) {
+        perror("project_1_queue");
+        exit(1);
+    }
+    ftruncate(fd_queue, chunk*sizeof(QueueData));
+    
     int fd_info = open("/tmp/project_1_info", O_RDWR | O_CREAT, 0644);
-    //ftruncate(fd, N*sizeof(int));
-
+    if (fd_info < 0) {
+        perror("project_1_info");
+        exit(1);
+    }
+    ftruncate(fd_info, sizeof(QueueInfo));
+    
     // memory mapping function instance
     //      address: NULL means the kernel can place the mapping anywhere it sees fit
     //      lenght: number of bytes to be mapped, the array will contain 10 integers
@@ -67,42 +80,32 @@ int main()
     QueueData *queue =  mmap(NULL, chunk*sizeof(QueueData), PROT_READ | PROT_WRITE,
         MAP_SHARED, fd_queue, 0);
     
-    
     QueueInfo *queue_info = mmap(NULL, sizeof(QueueInfo), PROT_READ | PROT_WRITE,
         MAP_SHARED, fd_info, 0);
+
     
-    /*
     // sem_filled begins in 0 because everuthing is empty
-    sem_t *sem_filled = malloc(sizeof(sem_t));
-    sem_init(sem_filled, 1, 0); 
+    sem_t sem_filled;
+    sem_init(&sem_filled, 1, chunk); 
     // sem_empty begins in {chunk} because everuthing is empty
-    sem_t *sem_empty = malloc(sizeof(sem_t));
-    sem_init(sem_empty, 1, chunk);
+    sem_t sem_empty;
+    sem_init(&sem_empty, 1, 0);
     int next_input = 0;
     int next_output = 0;
-    */
 
     // sem_filled begins in 0 because everuthing is empty
-    //sem_init(queue_info->sem_filled, 1, 0); 
-    // sem_empty begins in {chunk} because everuthing is empty
-    //sem_init(queue_info->sem_empty, 1, chunk);
-    //queue_info->next_input = 0;
-    //queue_info->next_output = 0;
-
-    /*
     queue_info->sem_filled = sem_filled;
+    // sem_empty begins in {chunk} because everuthing is empty
     queue_info->sem_empty = sem_empty;
     queue_info->next_input = 0;
     queue_info->next_output = 0;
-    */
-    
 
     // fill the queue with the data
-    //write_info(queue, queue_info);
+    //printf("Antes del llamado de la función");
+    write_info(queue, queue_info);
+    
    
-
     close(fd_queue);
     close(fd_info);
-
     return 0;
 }
